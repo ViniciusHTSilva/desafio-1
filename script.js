642af3c159777538321e01a02de30e782d54b894
Vinicius Henrique <88394501+ViniciusHTSilva@users.noreply.github.com>
2022-01-31 22:55:54 -0300

    criando ScrollReveal
    usando um pouco de JS e dando mais animação a pagina

---
+++ script.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({ reset : true});
+sr.reveal(".pes", {
+    duration: 1000 
+});
+
+
+sr.reveal(".publi", {
+    rotate:{ x: 0, y: 90, z: 0},
+    duration: 4000 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//const sr = ScrollReveal({
+  //  origin: 'top',
+    //distance: '50px',
+    //duration: 2000,
+    //reset: true
+//});
